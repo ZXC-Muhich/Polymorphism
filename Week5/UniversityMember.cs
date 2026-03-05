dbec0f0bff5589ab9211840e465a8584cb504ca5
mykyta <mykyta.sidorov@kse.org.ua>
2026-03-05 19:05:59 +0200

    Testing

--- Week5/UniversityMember.cs
+++ Week5/UniversityMember.cs
@@ -2,5 +2,14 @@
 
 public class UniversityMember
 {
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = string.IsNullOrEmpty(value) ? throw new Exception("Invalid Name") : value;
+    }
+    public string MemberId { get;  }
     
+     
 }
